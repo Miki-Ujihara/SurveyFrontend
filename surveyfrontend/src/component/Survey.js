923b4571f0a899f9a4d196597f018d8a80de1699
Miki-Ujihara <63641406+Miki-Ujihara@users.noreply.github.com>
2022-02-09 14:08:08 +0100

    design

--- surveyfrontend/src/component/Survey.js
+++ surveyfrontend/src/component/Survey.js
@@ -38,6 +38,15 @@ export function Survey() {
             setShowFailed(true)
         } else {
             setShowSend(true)
+            setQuestionOne("")
+            setQuestionTwo("")
+            setQuestionThree("")
+            setQuestionFour("")
+            setQuestionFive("")
+            setQuestionSix("")
+            setQuestionSeven("")
+            setQuestionEight("")
+            setQuestionNine("")
         }
     }
 
@@ -78,66 +87,66 @@ export function Survey() {
                 <Col>
                     <Form>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label className={"input-label"}><h4>1. Email</h4></Form.Label>
+                            <Form.Label className={"input-label"}><h5>1. Email*</h5></Form.Label>
                             <Form.Control onChange={e => setQuestionOne(e.target.value)} defaultValue={questionOne}
                                           className={"email"} type="email" placeholder="vorname.nachname@mgb.ch"/>
                         </Form.Group>
                         <br/>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label className={"input-label"}><h4>2. Motivation</h4></Form.Label>
+                            <Form.Label className={"input-label"}><h5>2. Motivation*</h5></Form.Label>
                             <br/>
                             <Rating onClick={handleQuestionTwo} className={"motivation"}
                                     ratingValue={questionTwo}/>
                         </Form.Group>
                         <br/>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                            <Form.Label className={"input-label"}><h4>3. Falls deine Motivation diese Woche drei oder
-                                weniger Sterne beträgt, bitte schreibe kurz warum:</h4></Form.Label>
+                            <Form.Label className={"input-label"}><h5>3. Falls deine Motivation diese Woche drei oder
+                                weniger Sterne beträgt, bitte schreibe kurz warum:</h5></Form.Label>
                             <Form.Control onChange={e => setQuestionThree(e.target.value)} defaultValue={questionThree}
                                           className="reasonMotivation" as="textarea"
                                           rows={3}/>
                         </Form.Group>
                         <br/>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label className={"input-label"}><h4>4. Team</h4></Form.Label>
+                            <Form.Label className={"input-label"}><h5>4. Team*</h5></Form.Label>
                             <br/>
                             <Rating onClick={handleQuestionFour} className={"team"}
                                     ratingValue={questionFour}/>
                         </Form.Group>
                         <br/>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label className={"input-label"}><h4>5. Falls deine Antwort bei Frage 5 drei oder
-                                weniger Sterne beträgt, bitte schreibe kurz warum:</h4></Form.Label>
+                            <Form.Label className={"input-label"}><h5>5. Falls deine Antwort bei Frage 5 drei oder
+                                weniger Sterne beträgt, bitte schreibe kurz warum:</h5></Form.Label>
                             <Form.Control onChange={e => setQuestionFive(e.target.value)} defaultValue={questionFive}
                                           className={"reasonTeam"} type="text"/>
                         </Form.Group>
                         <br/>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label className={"input-label"}><h4>6.
-                                Arbeiten</h4></Form.Label>
+                            <Form.Label className={"input-label"}><h5>6.
+                                Arbeiten*</h5></Form.Label>
                             <br/>
                             <Rating onClick={handleQuestionSix} className={"work"}
                                     ratingValue={questionSix}/>
                         </Form.Group>
                         <br/>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label className={"input-label"}><h4>7. Falls Du deine Leistung mit drei oder weniger
-                                Sternen beurteilt hast, bitte schreibe kurz warum:</h4></Form.Label>
+                            <Form.Label className={"input-label"}><h5>7. Falls Du deine Leistung mit drei oder weniger
+                                Sternen beurteilt hast, bitte schreibe kurz warum:</h5></Form.Label>
                             <Form.Control onChange={e => setQuestionSeven(e.target.value)} defaultValue={questionSeven}
                                           className={"reasonWork"} type="text"/>
                         </Form.Group>
                         <br/>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                            <Form.Label className={"input-label"}><h4>8. Wie war die Zusammenarbeit mit deiner/deinem
-                                Ausbildner*in?</h4></Form.Label>
+                            <Form.Label className={"input-label"}><h5>8. Wie war die Zusammenarbeit mit deiner/deinem
+                                Ausbildner*in?*</h5></Form.Label>
                             <Form.Control onChange={e => setQuestionEight(e.target.value)} defaultValue={questionEight}
                                           className="teamworkTrainer" as="textarea"
                                           rows={3}/>
                         </Form.Group>
                         <br/>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                            <Form.Label className={"input-label"}><h4>9. Falls deine Motivation diese Woche drei oder
-                                weniger Sterne beträgt, bitte schreibe kurz warum:</h4></Form.Label>
+                            <Form.Label className={"input-label"}><h5>9. Was Du noch sagen möchtest...
+                            </h5></Form.Label>
                             <Form.Control onChange={e => setQuestionNine(e.target.value)} defaultValue={questionNine}
                                           className="extraField" as="textarea"
                                           rows={3}/>
@@ -154,14 +163,15 @@ export function Survey() {
                         <BsFillCheckSquareFill/>
                         <strong className="me-auto">Info</strong>
                     </Toast.Header>
-                    <Toast.Body className={'text-white'}>Die Umfrage wurde abgesendet.</Toast.Body>
+                    <Toast.Body className={'text-white'}><h4>Die Umfrage wurde abgesendet.</h4></Toast.Body>
                 </Toast>
                 <Toast onClose={() => setShowFailed(false)} bg={"danger"} show={showFailed} delay={3000} autohide>
                     <Toast.Header>
                         <BsFillCheckSquareFill/>
                         <strong className="me-auto">Info</strong>
                     </Toast.Header>
-                    <Toast.Body className={'text-white'}><h5>Der/Die Praktikant*in existiert nicht oder die Eingaben sind ungültig.</h5></Toast.Body>
+                    <Toast.Body className={'text-white'}><h4>Der/Die Praktikant*in existiert nicht oder die Eingaben
+                        sind ungültig.</h4></Toast.Body>
                 </Toast>
             </ToastContainer>
         </>
